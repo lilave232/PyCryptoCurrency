635188fd586460c121e4e1d14410b80e7ebda034
Avery Pozzobon <averypozzobon@gmail.com>
2020-10-17 15:27:19 -0400

    Bug Fixes

--- cryptography/P2PNetNode.py
+++ cryptography/P2PNetNode.py
@@ -207,7 +207,7 @@ class P2PNetNode:
 
 					prev_hash = bytearray(32).hex()
 
-					if len(self.block_hashes) > i and i - 1 >= 0:
+					if len(self.block_hashes) > 0 and i - 1 >= 0:
 
 						prev_hash = self.block_hashes[i - 1]
 
