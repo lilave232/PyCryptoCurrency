1d3552ceedbbadc2049b049090d3d51f73106b18
Avery Pozzobon <averypozzobon@gmail.com>
2020-10-18 23:46:34 -0400

    Fixed Adjusting Target

--- cryptography/block_helper.py
+++ cryptography/block_helper.py
@@ -128,7 +128,7 @@ def gen_block(pubKeyHash, prev_block_hash,node): #GENERATE BLOCK
 
     txns.extend(node.txn_pool) #EXTEND ARRAY TO INCLUDE TXNS
 
-    blocks = {"prev_block_hash":prev_block_hash,"time":time_val,"target":node.target,"nonce":nonce,"txns":txns} #SETUP BLOCK FORMAT
+    blocks = {"prev_block_hash":prev_block_hash,"time":time_val,"target":node.node_target,"nonce":nonce,"txns":txns} #SETUP BLOCK FORMAT
 
     block_in_value = 0
     block_out_value = 0
